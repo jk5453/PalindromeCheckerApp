9a739ecf59fe1808aca83301052380dc0ba9c6e0
jk5453 <jk5453@srmist.edu.in>
2026-02-27 14:40:22 +0530

    Welcome Message for UC1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class PalindromeCheckerApp {
+
+/**
+ * Application entry point.
+ * This is a java Plindrome Checker App Welcome Message
+ *
+ * @author Developer
+ * @Version 1.0
+ */
+public static void main(String[] args){
+System.out.println("Welcome to the Palindrome Checker Management System");
+System.out.println("Version :1.0");
+System.out.println("System Initialized successfully.");
+}
+}
