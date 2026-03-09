265d2c1de5c78fbe5b7dd2272ff8f4606f218516
jk5453 <jk5453@srmist.edu.in>
2026-03-09 14:46:15 +0530

    UC9

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,35 +1,38 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Stack;
+
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
 
 public static void main(String[] args){
 
-    String input = "refer";
 
-    Deque<Character> deque = new ArrayDeque<>();
 
-    for (char c : input.toCharArray()) {
-        deque.addLast(c);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input string: ");
+        String str = sc.nextLine();
+
+        boolean result = isPalindrome(str, 0, str.length() - 1);
+
+        System.out.println("Is Palindrome: " + result);
+
+        sc.close();
     }
 
-    boolean isPalindrome = true;
 
-    while (deque.size() > 1) {
-        char first = deque.removeFirst();
-        char last = deque.removeLast();
+    private static boolean isPalindrome(String str, int start, int end) {
 
-        if (first != last) {
-            isPalindrome = false;
-            break;
+        if (start >= end) {
+            return true;
         }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
     }
-
-
-    System.out.println("Input: " + input);
-    System.out.println("Is Palindrome?: " + isPalindrome);
 }
-}
+
 
 
